04a92123868efea86b0dc9150bbf23c745e230f4
Hamza <echchablihamza1@gmail.com>
2026-03-04 09:35:50 +0000

    project init

---
+++ user-service/src/main/java/com/mediahub/user_service/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.mediahub.user_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/users/{id}")
+    public String getUserById(@PathVariable Long id) {
+
+
+        return "user 1" + id ;
+    }
+}
