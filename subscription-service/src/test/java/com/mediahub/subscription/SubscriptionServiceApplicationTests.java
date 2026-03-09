49eb1e58024cffe7f66c72745f5b2c907c0438b3
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2026-03-09 11:42:10 +0000

    Add Feign clients and WebClient configuration for inter-service communication

---
+++ subscription-service/src/test/java/com/mediahub/subscription/SubscriptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediahub.subscription;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class SubscriptionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
